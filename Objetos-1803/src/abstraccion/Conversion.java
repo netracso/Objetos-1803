47c315ed3b47296a267ad84f36d190b1fe9cc99c
netracso <39284159+netracso@users.noreply.github.com>
2018-05-22 02:13:39 -0500

    Update Conversion.java

--- Objetos-1803/src/abstraccion/Conversion.java
+++ Objetos-1803/src/abstraccion/Conversion.java
@@ -6,16 +6,14 @@ public class Conversion {
     private Metros metros;
     
     public float metrosAPies(){
-    
-    float  resultado = metros.getMetros()* 3.28f;
+    //Este metodo tiene logica
+    float  resultado = metros.getMetros() * 3.28f;
     return resultado;
     
     }
     public float centigradosAFarenheit() {
         //Este metodo tiene lógica
-        //error lo marca ya que por default el 1.8 lo lee como double
-
-        //Ponerle una f al final para identificar  que es un flotante
+       
         float resultado = grados.getCentigrados() * 1.8f + 32;
         return resultado;
 
