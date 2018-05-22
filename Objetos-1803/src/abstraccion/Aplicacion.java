ddd7a0e6d35f637ebb47538355a987338c79111a
netracso <39284159+netracso@users.noreply.github.com>
2018-05-22 02:10:37 -0500

    Update Aplicacion.java

--- Objetos-1803/src/abstraccion/Aplicacion.java
+++ Objetos-1803/src/abstraccion/Aplicacion.java
@@ -15,6 +15,14 @@ public class Aplicacion {
         //sout para mostrar System ctrl + barra espaciadora
           System.out.println( c.centigradosAFarenheit());
           
-          //creando  objeto
+          //creando  objeto para Metros
+        Metros m= new Metros();
+        //Se le asigna el valor a su atributo Metros
+        m.setMetros(10);
+        //Creando objeto para la clase conversion
+        Conversion p= new Conversion();
+        p.getMetros(m);
+        //Mostrando el resultado en pantalla
+        System.out.println(p.metrosAPies());
     }
 }
